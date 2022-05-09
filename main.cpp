3158bec899acc4af8f2fbea20293e1c53829a685
kernelai <redtomato158@gmail.com>
2022-05-09 03:00:38 +0000

    feaurre: set google format stytle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <iterator>
+
+void echo() {
+  std::copy(std::istream_iterator<char>(std::cin),
+            std::istream_iterator<char>(),
+            std::ostream_iterator<char>(std::cout, " "));
+}
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
-    return 0;
+  echo();
 }
